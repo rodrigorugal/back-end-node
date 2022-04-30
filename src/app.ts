c76272fb7fdda95c99939908e39e017aa7e50569
rodrigorugal <rodrigoluizandradedeoliveira@gmail.com>
2022-04-30 01:02:27 -0300

    fix/cors error

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import express from 'express';
-import routes from './routes';
+import express from "express";
+import routes from "./routes";
+
+const cors = require("cors");
 
 const app = express();
+app.use(cors);
 app.use(express.json());
 
-routes(app)
+routes(app);
 
-export default app
+export default app;
